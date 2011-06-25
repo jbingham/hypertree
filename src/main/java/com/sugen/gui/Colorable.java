51bfb0eada9bd3768d7d6a6ecada7f78830e0d4d
Jonathan Bingham <jb@jbingham.com>
2011-06-24 22:17:34 -0700

    new interfaces

---
+++ src/main/java/com/sugen/gui/Colorable.java
@@ -0,0 +1,11 @@
+package com.sugen.gui;
+
+import java.awt.Color;
+
+/**
+ * For color-able data models;
+ */
+public interface Colorable {
+	public Color getColor();
+	public void setColor(Color c);
+}
---
+++ src/main/java/com/sugen/gui/ToolTippable.java
@@ -0,0 +1,8 @@
+package com.sugen.gui;
+
+/**
+ * Tag for data that has a tooltip.
+ */
+public interface ToolTippable {
+	public String getToolTipText();
+}
